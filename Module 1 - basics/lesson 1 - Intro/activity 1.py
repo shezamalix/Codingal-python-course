1a4526910c992727a4d28cdde6e1226fa7c9b421
Sheza Malik <sheza.malix@gmail.com>
2026-07-27 18:16:43 +0300

    modified activity 1

--- Module 1 - basics/lesson 1 - Intro/activity 1.py
+++ Module 1 - basics/lesson 1 - Intro/activity 1.py
@@ -2,3 +2,7 @@ print("Today I am getting started with Python")
 print("My name is Sheza")
 print(10+40)
 print("The result  of 129 * 12367 =" , 129 * 12367)
+
+#creating a variable called age
+age = 14
+print(age)

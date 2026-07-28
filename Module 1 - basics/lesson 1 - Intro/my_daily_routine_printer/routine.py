9b78e51018026d818a7ddfcdd0dc5731182bed62
Sheza Malik <sheza.malix@gmail.com>
2026-07-29 02:58:32 +0300

    my daily routine project

---
+++ Module 1 - basics/lesson 1 - Intro/my_daily_routine_printer/routine.py
@@ -0,0 +1,16 @@
+print("My daily summer routine!")
+
+print("8:00 - 8:30 : I wake up and freshen up")
+print("8:30 - 9:00 : I eat breakfast and clean my room")
+print("9:00 - 11:00 : I study")
+print("11:00 - 3:00 : I enjoy productive hobbies such as reading,painting etc")
+print("3:00 - 4:00 : I prepare and eat lunch")
+print("4:00 - 6:00 : I call my friend")
+print("6:00 - 10:00 : I play fortnite")
+print("10:00 - 11:00 : prepare for bed")
+print("11:00 : I go to bed")
+
+study_hours = 2 * 7
+print("In a week, I study for a total of" ,study_hours ,"hours")
+
+

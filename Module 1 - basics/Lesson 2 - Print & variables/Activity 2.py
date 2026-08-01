86579087c311cec42b0dacfaeac47b65a5f74e22
Sheza Malik <sheza.malix@gmail.com>
2026-08-01 20:19:14 +0300

    lesson 2 acitivity 2 & 3

---
+++ Module 1 - basics/Lesson 2 - Print & variables/Activity 2.py
@@ -0,0 +1,11 @@
+favorite_food = "Pizza"
+print(favorite_food)
+food = input("What is your favorite food?\n")
+print(f"The user likes {food}")
+_food2 = "pasta"
+
+
+import keyword
+print(keyword.kwlist)
+
+# assert = 5 

9a5006aefe44662c646e2c2fb4afa746753f4c8d
Sheza Malik <sheza.malix@gmail.com>
2026-08-01 21:43:05 +0300

    Project - Storing Birthdays

---
+++ Module 1 - basics/L2 - Print & variables/Project - storing birthdays.py
@@ -0,0 +1,7 @@
+selina_bday = "11th Nov 2012"
+Mariam_bday = "9th Apr 2012"
+Haya_bday = "12th Jun 2012"
+Hareem_bday = "15th May 2012"
+Minn_bday = "29th Dec 2012"
+
+print(f"Selinas birthday is on {selina_bday}. Mariams birthday is on {Mariam_bday}. Haya's birthday is on {Haya_bday}.Hareems birthday is on {Hareem.bday}.")

d8f4914eb29d73e4b6ceed96009461759e959b62
Sheza Malik <sheza.malix@gmail.com>
2026-08-01 19:58:00 +0300

    Finished L2, A1

---
+++ Module 1 - basics/Lesson 2 - Print & variables/Activity 1.py
@@ -0,0 +1,12 @@
+print("Welcome to the world of programming")
+print("2011")
+print(2011)
+print("this is line 1\n\n\n\n")
+print("This is line 2")
+
+name = "Sheza"
+print("My name is", name)
+print(F"my name is {name}")
+
+print("blue", end="🥀")
+print("termite")

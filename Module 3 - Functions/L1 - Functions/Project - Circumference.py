654fa513d27b6479c39ed206f75ae18322758c7d
Sheza Malik <sheza.malix@gmail.com>
2026-08-31 19:42:40 +0300

    Project - circumference and area

--- Module 3 - Functions/L1 - Functions/Project - Circumference.py
+++ Module 3 - Functions/L1 - Functions/Project - Circumference.py
@@ -7,3 +7,10 @@ def area(radius, pi):
     print(f"the area of the circle is {(radius ** 2) * pi}")
 
 area("",3.14159)
+
+def cirumference(pi):
+    radius =  int(input("Enter the radius of the circle : "))
+    diameter = radius * 2
+    print(f"the cirumference of the circle is {diameter * pi}")
+
+cirumference(3.14159)
---
+++ Module 3 - Functions/L2 - Arguements/Activity 3.py
@@ -0,0 +1,7 @@
+#Called a recusrive function call(call the same function inside the function)
+def hello():
+    print("Hello")
+    hello()  
+
+hello()  
+

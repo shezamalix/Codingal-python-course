f9463b2aeb969f155d4a1cd65e74aa44e42f5ffd
Sheza Malik <sheza.malix@gmail.com>
2026-09-07 18:05:39 +0300

    Activity 2.py

---
+++ Module 3 - Functions/L5 - Random & math module/Acvtivity 2.py
@@ -0,0 +1,19 @@
+import math
+
+n = 16
+print(math.sqrt(n))# math = module  # sqrt = function
+
+print(math.floor(5.75)) #rounds off to lower integer, 5
+
+print(math.ceil(5.75)) # rounds off to upper integer, 6
+
+print(math.fabs(-732)) #absolute function, removes sign(removes + and -)
+
+print(math.gcd(12,20))
+
+print(math.lcm(12,20))
+
+print(math.copysign(-10,20))# copies sign form the 2nd number to the first
+
+math.
+

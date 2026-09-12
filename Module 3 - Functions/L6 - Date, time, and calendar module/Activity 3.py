f6b244d2ec67af2d444d10681e16dc3da180a50e
Sheza Malik <sheza.malix@gmail.com>
2026-09-12 20:21:07 +0300

    Activity 3

--- Module 3 - Functions/L6 - Date, time, and calendar module/Activity 3.py
+++ Module 3 - Functions/L6 - Date, time, and calendar module/Activity 3.py
@@ -1,13 +1,26 @@
 # M3 L6 A3
 
 # 1) Define a function `hotel_cost(nights)` that calculates hotel charges:
+def hotel_cost(nights):
+    return 140 * nights
 
+
+    
 # a) Multiply 140 by the number of nights.
 
 # b) Return the total hotel cost.
 
 # 2) Define a function `plane_ride_cost(city)` that returns airfare based on the city:
 
+def plane_ride_cost(city):
+    if city == "Charlotte" :
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
 # a) If city is "Charlotte", return 183.
 
 # b) Else if city is "Tampa", return 220.
@@ -17,6 +30,16 @@
 # d) Else if city is "Los Angeles", return 475.
 
 # 3) Define a function `rental_car_cost(days)` that calculates car rental cost:
+def rental_car_cost(days):
+    base_cost = 40
+
+    if days >= 7:
+        return (days * base_cost) - 50
+
+    elif days >= 3 :
+        return (days * base_cost) - 20
+    else:
+        return base_cost * days
 
 # a) Base cost is 40 per day.
 
@@ -30,6 +53,17 @@
 
 # 4) Define a function `trip_cost(city, days, spending_money)` to calculate total trip cost:
 
+def trip_cost(city, days, spending_money) :
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+desired_city = input("What city would you like to vist? ")
+desired_nights = int(input("How many nights would you like to stay? "))
+desired_spending_money = int(input("How much would you like to spend? "))
+
+print(f"your total car rental cost = {rental_car_cost(desired_nights)}")
+print(f"your total hotel cost = {hotel_cost(desired_nights)}")
+print(f"your total plane ride cost = {plane_ride_cost(desired_city)}")
+print(f"your total trip cost = {trip_cost(desired_city, desired_nights, desired_spending_money)}")
 # a) Add rental car cost for `days`.
 
 # b) Add hotel cost for `days`.
